b09122892b85dbb1006ba7ab8b8e5a71d71db635
root <root@alex>
2016-09-21 23:46:55 +0300

    исправил название сортир. пузырьком

--- e_data/bubble_sort.h
+++ e_data/bubble_sort.h
@@ -1,10 +1,10 @@
-#ifndef _BUMBLE_SORT_H_
-#define _BUMBLE_SORT_H_
+#ifndef _BUBBLE_SORT_H_
+#define _BUBBLE_SORT_H_
 
 #include "swap.h"
 // O(n ^ 2)
 template<typename T, typename Type>
-void bumble_sort(T * m, Type size)
+void bubble_sort(T * m, Type size)
 {
     for(int i = 0; i < size; ++i)
         for(int j = 0; j < size - 1; ++j)
