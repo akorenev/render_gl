a2ba5eae7c3597eab32e46e91722a3cdb1b93ccc
alexander <korenev.alexander@gmail.com>
2017-08-15 16:54:07 +0300

    выбор точки

--- render.cpp
+++ render.cpp
@@ -1,5 +1,5 @@
 #include "render.h"
-#include <QDebug>
+#include <iostream>
 
 #include "plypainter.h"
 #include "painter.h"
@@ -118,6 +118,80 @@ const double &render::getZoom() const
 
 void render::updatePosition(int x, int y, int _x, int _y)
 {
+    if(m_painterL.empty())
+        return;
+    QMatrix4x4 m;
+    m.setToIdentity();
+    m.translate(m_currentPos.x(), m_currentPos.y(), m_boundingBox.min.z());
+    m.scale(m_zoom, m_zoom, m_zoom);
+    m.rotate(m_rotate_x, 1.0, 0.0, 0.0);
+    m.rotate(m_rotate_y, 0.0, 1.0, 0.0);
+    m.rotate(m_rotate_z, 0.0, 0.0, 1.0);
+
+    QMatrix4x4 m1;
+    m1.setToIdentity();
+    m1.perspective(45, m_ratio, 1, 10000);
+
+    QRect viewport(0, 0, m_width, m_height);
+    if(!m_item_t)
+    {
+        QVector3D vv;
+        vv[0] = x;
+        vv[1] = m_height - _y;
+        vv[2] = -1;
+        QVector3D v0 = vv.unproject(m, m1, viewport);
+        vv[0] = x;
+        vv[1] = m_height - _y;
+        vv[2] = 1;
+        QVector3D v1 = vv.unproject(m, m1, viewport);
+        v1 = v1 - v0;
+        v1.normalize();
+
+        Item_tL item_tL;
+        RayPoint ray(v0, v1);
+
+        for(auto & v : m_painterL)
+        {
+            Model * model = v->getModel();
+            if(Item_t::Ptr item_t = model->searchFirstItem(ray))
+                item_tL.push_back(item_t);
+
+        }
+        if(item_tL.empty())
+            return;
+        item_tL.sort(SortItem_t);
+        m_item_t = *item_tL.begin();
+
+        PointItem * pointItem = (PointItem*)m_item_t->item;
+        unsigned char color[4] = {0, 255, 0, 255};
+        pointItem->setFill(color);
+        std::cout << "selected item " << item_tL.size() << std::endl;
+    }
+
+    const QVector3D & v = m_item_t->v;
+
+    QVector3D v0;
+    v0[0] = v[0];
+    v0[1] = v[1];
+    v0[2] = v[2];
+    QVector3D vv(v0.project(m, m1, viewport));
+
+    QVector3D v1;
+    v1[0] = x;
+    v1[1] = m_height - y;
+    v1[2] = vv[2];
+    QVector3D vv1(v1.unproject(m, m1, viewport));
+
+    QVector3D v2;
+    v2[0] = _x;
+    v2[1] = m_height - _y;
+    v2[2] = vv[2];
+    QVector3D vv2(v2.unproject(m, m1, viewport));
+
+    m_currentPos[0] += m_zoom *(vv1[0] - vv2[0]);
+    m_currentPos[1] += m_zoom *(vv1[1] - vv2[1]);
+    m_currentPos[2] += m_zoom *(vv1[2] - vv2[2]);
+    emit update();
 //    QMatrix4x4 m;
 //    m.setToIdentity();
 //    m.translate(m_currentPos.x(), m_currentPos.y(), m_boundingBox.min.z());
@@ -205,6 +279,7 @@ void render::updatePosition(int x, int y, int _x, int _y)
 
 //    //qDebug() << x1 - x2 << "  " <<  z1 - z2;
 //    emit update();
+  
 }
 
 const PointD &render::getPos() const
@@ -311,9 +386,11 @@ void render::releaseMouse()
     if(!m_item_t)
         return;
     unsigned char color[4] = {255, 255, 255, 255};
-//    PointItem * pointItem = (PointItem*)m_item_t->item;
-//    pointItem->setFill(color);
+    PointItem * pointItem = (PointItem*)m_item_t->item;
+    if(pointItem)
+       pointItem->setFill(color);
     m_item_t = 0;
+    emit update();
 
 }
 
